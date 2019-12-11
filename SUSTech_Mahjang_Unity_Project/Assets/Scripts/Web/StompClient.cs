a07dcc9f7aaf5bdbbe6403581ee528cc2c20e4d2
IF-ACT <xyyjxl@outlook.com>
2019-12-11 21:46:14 +0800

    update Stomp

--- SUSTech_Mahjang_Unity_Project/Assets/Scripts/Web/StompClient.cs
+++ SUSTech_Mahjang_Unity_Project/Assets/Scripts/Web/StompClient.cs
@@ -2,25 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Net.WebSockets;
+using UnityEngine;
 
 namespace Assets.Scripts.Web
 {
 	public class StompClient
 	{
+		private ArraySegment<Byte> receive;
+		private TimeSpan delay = TimeSpan.FromSeconds(5);
 		private ClientWebSocket client;
 		private const string acceptVersion = "1.1,1.0";
 		private const string heartBeat = "0,0";
-		private System.Threading.CancellationToken CancellationToken = new System.Threading.CancellationToken(false);
+		private CancellationToken cancellationToken = new System.Threading.CancellationToken(false);
+
 		public StompClient(Uri uri)
 		{
 			client = new ClientWebSocket();
-			Task c = client.ConnectAsync(uri, CancellationToken);
-			c.Wait();
+			Task c = client.ConnectAsync(uri, cancellationToken);
+			c.Wait(delay);
+			if (!c.IsCompleted)
+			{
+				throw new WebSocketException("Failed to establish websocket");
+			}
 		}
-
-		
 
 		public async Task Connect()
 		{
@@ -28,6 +35,49 @@ namespace Assets.Scripts.Web
 			msg.AddHead("accept-version", acceptVersion);
 			msg.AddHead("heart-beat", heartBeat);
 			await SendWebSocket(msg);
+
+			await HandleWebSocket(client);
+		}
+
+		private async Task HandleWebSocket(WebSocket socket)
+		{
+			const int maxMessageSize = 1024;
+			byte[] receiveBuffer = new byte[maxMessageSize];
+
+			while (socket.State == WebSocketState.Open)
+			{
+				WebSocketReceiveResult receiveResult = await socket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+
+				if (receiveResult.MessageType == WebSocketMessageType.Close)
+				{
+					await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+				}
+				else if (receiveResult.MessageType == WebSocketMessageType.Binary)
+				{
+					await socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "Cannot accept binary frame", CancellationToken.None);
+				}
+				else
+				{
+					int count = receiveResult.Count;
+
+					while (receiveResult.EndOfMessage == false)
+					{
+						if (count >= maxMessageSize)
+						{
+							string closeMessage = string.Format("Maximum message size: {0} bytes.", maxMessageSize);
+							await socket.CloseAsync(WebSocketCloseStatus.MessageTooBig, closeMessage, CancellationToken.None);
+							return;
+						}
+
+						receiveResult = await socket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer, count, maxMessageSize - count), CancellationToken.None);
+						count += receiveResult.Count;
+					}
+
+					var receivedString = Encoding.UTF8.GetString(receiveBuffer, 0, count);
+
+					Debug.Log(receivedString);
+				}
+			}
 		}
 
 		public async Task DisConnect()
@@ -35,7 +85,7 @@ namespace Assets.Scripts.Web
 			var msg = new StompFrame(ClientCommand.DISCONNECT);
 			Task t = SendWebSocket(msg);
 			t.Wait(1000);
-			await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken);
+			await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
 		}
 
 		public async Task Subscribe(string dst)
@@ -61,7 +111,7 @@ namespace Assets.Scripts.Web
 				new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg.ToString())),
 				WebSocketMessageType.Text,
 				true,
-				CancellationToken
+				CancellationToken.None
 			);
 		}
 	}
--- SUSTech_Mahjang_Unity_Project/Assets/Tests/Stomp.cs
+++ SUSTech_Mahjang_Unity_Project/Assets/Tests/Stomp.cs
@@ -8,26 +8,30 @@ using System.Threading.Tasks;
 
 namespace Tests
 {
+	[TestFixture]
     public class Stomp
     {
         // A Test behaves as an ordinary method
         [Test]
         public void Connect()
         {
+			Debug.Log("hey");
+			Task t = ConectTest();
+			t.Wait(10000);
+        }
+
+		async Task ConectTest()
+		{
 			var client = new StompClient(new System.Uri("ws://10.21.63.106:8083/ws/websocket"));
-			Task task = client.Connect();
-			task.Wait(10000);
-			task = client.Subscribe("/topic/public");
-			task.Wait(1000);
-			for(int i = 0; i<10; i++)
+			await client.Connect();
+			await client.Subscribe("/topic/echo");
+			for (int i = 0; i < 10; i++)
 			{
 				//task = client.Send();
-				task = client.Send("/app/public.sendMessage", "{\"sender\":\"Unity\",\"type\":\"CHAT\",\"content\":\"ko no DIO da!!!\"}");
-				task.Wait(1000);
+				await client.Send("/app/echo", "{\"sender\":\"Unity\",\"type\":\"CHAT\",\"content\":\"ko no DIO da!!!\"}");
 			}
-			task = client.DisConnect();
-			task.Wait(1000);
-        }
+			await client.DisConnect();
+		}
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
