0a0dcb5409993445e7b75614f7ad8476b2b42a20
Seeyn <11710503@mail.sustech.edu.cn>
2019-12-12 11:00:33 +0800

    ..

--- SUSTech_Mahjang_Unity_Project/Assets/Scripts/HandTileOthers.cs
+++ SUSTech_Mahjang_Unity_Project/Assets/Scripts/HandTileOthers.cs
@@ -29,9 +29,13 @@ public class HandTileOthers : MonoBehaviour
 
     public void AddTile(Tile tile)
     {
+<<<<<<< HEAD
 
         myplayer.hand.Add(tile);
         myplayer.hand.Sort();
+=======
+        myplayer.hand.Add(tile);
+>>>>>>> parent of e32f7c8... Merge branch 'master' of https://github.com/Tchao111/SUSTechMajhong
         Reconstruct();
     }
 
