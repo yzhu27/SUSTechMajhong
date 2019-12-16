f64584b97eeb8b443e0c91b94a616bb8d2882091
IF-ACT <xyyjxl@outlook.com>
2019-12-16 21:20:29 +0800

    Add empty Charactor

---
+++ SUSTech_Mahjang_Unity_Project/Assets/Scripts/GameMain/Charactors/EmptyCharactor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.GameMain.Charactors
+{
+	public class EmptyCharactor : Charactor
+	{
+		public override uint id { get => 0; }
+		public override string name { get => "Empty"; }
+		public EmptyCharactor(Player player) : base(player) { }
+
+	}
+}
