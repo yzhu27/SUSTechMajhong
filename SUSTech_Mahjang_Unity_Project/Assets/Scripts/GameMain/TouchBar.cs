0b931f40ebbdb556e243cc8f0e39107eb9ec7265
Seeyn <11710503@mail.sustech.edu.cn>
2019-12-18 23:47:16 +0800

    tiny change

--- SUSTech_Mahjang_Unity_Project/Assets/Scripts/GameMain/TouchBar.cs
+++ SUSTech_Mahjang_Unity_Project/Assets/Scripts/GameMain/TouchBar.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.GameMain
 {
@@ -11,40 +13,5 @@ namespace Assets.Scripts.GameMain
 		Eat, Touch, Rod, Win, Swap
 	}
 
-	public class TouchBar
-	{
-		public bool eatButton { get; private set; }
-		public bool touchButton { get; private set; }
-		public bool rodButton { get; private set; }
-		public bool winButton { get; private set; }
-		public bool swapButton { get; private set; }
-
-		public TouchBar()
-		{
-			eatButton = false;
-			touchButton = false;
-			rodButton = false;
-			winButton = false;
-		}
-
-		/// <summary>
-		/// 更新行为可行性
-		/// </summary>
-		/// <param name="actions">
-		///		从<see cref="MainPlayer.GetActionsOnPlay"/>
-		///		或<see cref="MainPlayer.GetActionsOnResponse"/>
-		///		获取的返回值
-		/// </param>
-		public void SetActions(HashSet<Action> actions)
-		{
-			eatButton = actions.Contains(Action.Eat);
-			touchButton = actions.Contains(Action.Touch);
-			rodButton = actions.Contains(Action.Rod);
-			winButton = actions.Contains(Action.Win);
-			swapButton = actions.Contains(Action.Swap);
-		}
-
-		
-
-	}
+	
 }
