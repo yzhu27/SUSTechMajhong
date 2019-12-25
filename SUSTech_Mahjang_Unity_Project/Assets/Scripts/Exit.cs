5523fb19ad24465aca090701c7cd6b993bf32e09
Seeyn <11710503@mail.sustech.edu.cn>
2019-12-26 04:41:25 +0800

    success!!!

---
+++ SUSTech_Mahjang_Unity_Project/Assets/Scripts/Exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public void click()
+    {
+        Application.Quit();
+    }
+   
+}
